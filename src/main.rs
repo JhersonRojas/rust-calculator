00d62ebdf3906815ad5294c2b41f2314d8e534ef
JhersonRojas <jhersonrojas.dev@gmail.com>
2023-11-10 15:30:04 -0500

    aprendiendo a recibir un input por consola para hacer las operaciones

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 // Importación de los metodos como modulo
 mod operaciones;
 
+use std::io;
+
 // Función main principal que se ejecuta en Rust
 fn main() {
     println!("Metodos de ejemplo para la calculadora sencilla");
 
-    println!("La suma es {}", operaciones::suma(9, 5));
-    println!("La resta es {}", operaciones::resta(2, 5));
-    println!("La división es {}", operaciones::division(7, 5));
-    println!("La multiplicación es {}", operaciones::multiplicacion(8, 5));
+    println!("Ingrese el primer número:");
+    let mut entrada1 = String::new();
+    io::stdin()
+        .read_line(&mut entrada1)
+        .expect("Error al leer la entrada");
+    let num1: i32 = entrada1.trim().parse().expect("Ingrese un número válido");
+
+    println!("Ingrese el segundo número:");
+    let mut entrada2 = String::new();
+    io::stdin()
+        .read_line(&mut entrada2)
+        .expect("Error al leer la entrada");
+    let num2: i32 = entrada2.trim().parse().expect("Ingrese un número válido");
+
+    println!(
+        "El resultado de la suma es {}",
+        operaciones::suma(num1, num2)
+    );
+    println!(
+        "El resultado de la resta es {}",
+        operaciones::resta(num1, num2)
+    );
+    println!(
+        "El resultado de la división es {}",
+        operaciones::division(num1, num2)
+    );
+    println!(
+        "El resultado de la multiplicación es {}",
+        operaciones::multiplicacion(num1, num2)
+    );
 }
